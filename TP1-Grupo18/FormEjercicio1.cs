3151245ec25af9f728732903516ed9a24a8e483c
GitHubestudio <176661583+GitHubestudio@users.noreply.github.com>
2025-03-25 18:08:31 -0300

    agregue validacion en listbox

--- TP1-Grupo18/FormEjercicio1.cs
+++ TP1-Grupo18/FormEjercicio1.cs
@@ -28,8 +28,33 @@ namespace TP1_Grupo18
         {
             if(txtNombre.Text.Trim() != "")
             {
-                lbNombres.Items.Add(txtNombre.Text.Trim());
-                txtNombre.Text = string.Empty;
+                //se crea una bandera y una copia del texto para hacer las verificaciones
+                string texto = txtNombre.Text.Trim().ToLower();
+                bool existe = false;
+
+                //se compara cada item de la lista con la copia
+                foreach (string item in lbNombres.Items)
+                {
+                    //si encuentra coincidencia cambia de estado la bandera y detiene el bucle
+                    if (item.ToLower() == texto)
+                    {
+                        existe = true;
+                        break;
+                    }
+                }
+
+                //se verifica si el nombre ya fue ingresado
+                if (existe)
+                {
+                    MessageBox.Show("El nombre ya fue ingresado", "Ventana Emergente");
+                }
+                //se agrega el contenido del texto original a la lista de items
+                else
+                {
+                    lbNombres.Items.Add(txtNombre.Text.Trim());
+                }
+                //se limpia el cuadro de texto
+                txtNombre.Text = "";
             }
         }
     }
