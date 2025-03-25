97f7d1ad01efdeec9d286f8a3bfca95f91964671
Alanjaqs <alannjaqs@gmail.com>
2025-03-25 11:31:02 -0300

    Form principal

---
+++ TP1-Grupo18/FormPrincipal.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace TP1_Grupo18
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnE1 = new System.Windows.Forms.Button();
+            this.btnE2 = new System.Windows.Forms.Button();
+            this.btnE3 = new System.Windows.Forms.Button();
+            this.lblIntegrantes = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnE1
+            // 
+            this.btnE1.BackColor = System.Drawing.SystemColors.Control;
+            this.btnE1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnE1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnE1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnE1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnE1.Location = new System.Drawing.Point(54, 72);
+            this.btnE1.Name = "btnE1";
+            this.btnE1.Size = new System.Drawing.Size(114, 39);
+            this.btnE1.TabIndex = 0;
+            this.btnE1.Text = "EJERCICIO 1";
+            this.btnE1.UseVisualStyleBackColor = false;
+            this.btnE1.Click += new System.EventHandler(this.btnE1_Click);
+            // 
+            // btnE2
+            // 
+            this.btnE2.BackColor = System.Drawing.SystemColors.Control;
+            this.btnE2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnE2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnE2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnE2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnE2.Location = new System.Drawing.Point(231, 72);
+            this.btnE2.Name = "btnE2";
+            this.btnE2.Size = new System.Drawing.Size(114, 39);
+            this.btnE2.TabIndex = 1;
+            this.btnE2.Text = "EJERCICIO 2";
+            this.btnE2.UseVisualStyleBackColor = false;
+            // 
+            // btnE3
+            // 
+            this.btnE3.BackColor = System.Drawing.SystemColors.Control;
+            this.btnE3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnE3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnE3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnE3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnE3.Location = new System.Drawing.Point(410, 72);
+            this.btnE3.Name = "btnE3";
+            this.btnE3.Size = new System.Drawing.Size(114, 39);
+            this.btnE3.TabIndex = 2;
+            this.btnE3.Text = "EJERCICIO 3";
+            this.btnE3.UseVisualStyleBackColor = false;
+            // 
+            // lblIntegrantes
+            // 
+            this.lblIntegrantes.AutoSize = true;
+            this.lblIntegrantes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIntegrantes.Location = new System.Drawing.Point(50, 171);
+            this.lblIntegrantes.Name = "lblIntegrantes";
+            this.lblIntegrantes.Size = new System.Drawing.Size(107, 20);
+            this.lblIntegrantes.TabIndex = 3;
+            this.lblIntegrantes.Text = "Integrantes:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(155, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Jaques Alan Federico";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(155, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Silguero Jonathan Leonardo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(155, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(224, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Contreras Lucero Julian Yago";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(155, 284);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Coronado Lucas Matias";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(155, 311);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Sosa Nicolas Esteban";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(155, 336);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Segovia Enzo";
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(574, 412);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblIntegrantes);
+            this.Controls.Add(this.btnE3);
+            this.Controls.Add(this.btnE2);
+            this.Controls.Add(this.btnE1);
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulario Principal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnE1;
+        private System.Windows.Forms.Button btnE2;
+        private System.Windows.Forms.Button btnE3;
+        private System.Windows.Forms.Label lblIntegrantes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ TP1-Grupo18/FormPrincipal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP1_Grupo18
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnE1_Click(object sender, EventArgs e)
+        {
+            FormEjercicio1 formEjercicio1 = new FormEjercicio1(this);
+            formEjercicio1.Show();
+            this.Hide();
+        }
+    }
+}
