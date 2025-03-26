c1005e61d2d47a30b24c91f7c789024b64c690c9
julcontrerass <jjulian.contrerass@gmail.com>
2025-03-26 09:49:21 -0300

    Inicio de ejercicio 2 con ingresos de nombre y apellido y boton agregar

--- TP1-Grupo18/FormEjercicio2.Designer.cs
+++ TP1-Grupo18/FormEjercicio2.Designer.cs
@@ -28,23 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textApellido = new System.Windows.Forms.TextBox();
+            this.buttonAgregar = new System.Windows.Forms.Button();
+            this.lblIngresoDeDatos = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(38, 155);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(69, 17);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            this.lblNombre.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(38, 187);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(71, 17);
+            this.lblApellido.TabIndex = 1;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(114, 155);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(147, 20);
+            this.textNombre.TabIndex = 2;
+            this.textNombre.TextChanged += new System.EventHandler(this.textNombre_TextChanged);
+            // 
+            // textApellido
+            // 
+            this.textApellido.Location = new System.Drawing.Point(114, 186);
+            this.textApellido.Name = "textApellido";
+            this.textApellido.Size = new System.Drawing.Size(147, 20);
+            this.textApellido.TabIndex = 3;
+            // 
+            // buttonAgregar
+            // 
+            this.buttonAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAgregar.Location = new System.Drawing.Point(78, 237);
+            this.buttonAgregar.Name = "buttonAgregar";
+            this.buttonAgregar.Size = new System.Drawing.Size(145, 39);
+            this.buttonAgregar.TabIndex = 4;
+            this.buttonAgregar.Text = "Agregar";
+            this.buttonAgregar.UseVisualStyleBackColor = true;
+            this.buttonAgregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblIngresoDeDatos
+            // 
+            this.lblIngresoDeDatos.AutoSize = true;
+            this.lblIngresoDeDatos.Location = new System.Drawing.Point(38, 9);
+            this.lblIngresoDeDatos.Name = "lblIngresoDeDatos";
+            this.lblIngresoDeDatos.Size = new System.Drawing.Size(86, 13);
+            this.lblIngresoDeDatos.TabIndex = 5;
+            this.lblIngresoDeDatos.Text = "Ingreso de datos";
+            this.lblIngresoDeDatos.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // FormEjercicio2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(584, 418);
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(769, 487);
+            this.Controls.Add(this.lblIngresoDeDatos);
+            this.Controls.Add(this.buttonAgregar);
+            this.Controls.Add(this.textApellido);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
             this.Name = "FormEjercicio2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Nombre y apellido";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormEjercicio2_FormClosed);
             this.Load += new System.EventHandler(this.FormEjercicio2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textApellido;
+        private System.Windows.Forms.Button buttonAgregar;
+        private System.Windows.Forms.Label lblIngresoDeDatos;
     }
 }
