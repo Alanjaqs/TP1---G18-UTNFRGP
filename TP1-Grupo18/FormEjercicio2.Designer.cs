f40214424bed689f0afa174d857f78ce66fc288d
Nicolas-Sosa1 <nicolassosaesteban2003@gmail.com>
2025-03-27 10:53:49 -0300

    agregando validacion ListBox

--- TP1-Grupo18/FormEjercicio2.Designer.cs
+++ TP1-Grupo18/FormEjercicio2.Designer.cs
@@ -33,14 +33,18 @@
             this.textNombre = new System.Windows.Forms.TextBox();
             this.textApellido = new System.Windows.Forms.TextBox();
             this.buttonAgregar = new System.Windows.Forms.Button();
-            this.lblIngresoDeDatos = new System.Windows.Forms.Label();
+            this.lbIngresoNombres = new System.Windows.Forms.ListBox();
+            this.grbIngresoDeDatos = new System.Windows.Forms.GroupBox();
+            this.grbElementos = new System.Windows.Forms.GroupBox();
+            this.grbIngresoDeDatos.SuspendLayout();
+            this.grbElementos.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
             this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombre.Location = new System.Drawing.Point(38, 155);
+            this.lblNombre.Location = new System.Drawing.Point(70, 125);
             this.lblNombre.Name = "lblNombre";
             this.lblNombre.Size = new System.Drawing.Size(69, 17);
             this.lblNombre.TabIndex = 0;
@@ -51,7 +55,7 @@
             // 
             this.lblApellido.AutoSize = true;
             this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblApellido.Location = new System.Drawing.Point(38, 187);
+            this.lblApellido.Location = new System.Drawing.Point(70, 176);
             this.lblApellido.Name = "lblApellido";
             this.lblApellido.Size = new System.Drawing.Size(71, 17);
             this.lblApellido.TabIndex = 1;
@@ -59,7 +63,7 @@
             // 
             // textNombre
             // 
-            this.textNombre.Location = new System.Drawing.Point(114, 155);
+            this.textNombre.Location = new System.Drawing.Point(158, 125);
             this.textNombre.Name = "textNombre";
             this.textNombre.Size = new System.Drawing.Size(147, 20);
             this.textNombre.TabIndex = 2;
@@ -67,7 +71,7 @@
             // 
             // textApellido
             // 
-            this.textApellido.Location = new System.Drawing.Point(114, 186);
+            this.textApellido.Location = new System.Drawing.Point(158, 175);
             this.textApellido.Name = "textApellido";
             this.textApellido.Size = new System.Drawing.Size(147, 20);
             this.textApellido.TabIndex = 3;
@@ -75,7 +79,7 @@
             // buttonAgregar
             // 
             this.buttonAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonAgregar.Location = new System.Drawing.Point(78, 237);
+            this.buttonAgregar.Location = new System.Drawing.Point(118, 233);
             this.buttonAgregar.Name = "buttonAgregar";
             this.buttonAgregar.Size = new System.Drawing.Size(145, 39);
             this.buttonAgregar.TabIndex = 4;
@@ -83,15 +87,37 @@
             this.buttonAgregar.UseVisualStyleBackColor = true;
             this.buttonAgregar.Click += new System.EventHandler(this.button1_Click);
             // 
-            // lblIngresoDeDatos
+            // lbIngresoNombres
             // 
-            this.lblIngresoDeDatos.AutoSize = true;
-            this.lblIngresoDeDatos.Location = new System.Drawing.Point(38, 9);
-            this.lblIngresoDeDatos.Name = "lblIngresoDeDatos";
-            this.lblIngresoDeDatos.Size = new System.Drawing.Size(86, 13);
-            this.lblIngresoDeDatos.TabIndex = 5;
-            this.lblIngresoDeDatos.Text = "Ingreso de datos";
-            this.lblIngresoDeDatos.Click += new System.EventHandler(this.label1_Click_1);
+            this.lbIngresoNombres.FormattingEnabled = true;
+            this.lbIngresoNombres.Location = new System.Drawing.Point(41, 29);
+            this.lbIngresoNombres.Name = "lbIngresoNombres";
+            this.lbIngresoNombres.Size = new System.Drawing.Size(244, 316);
+            this.lbIngresoNombres.TabIndex = 6;
+            // 
+            // grbIngresoDeDatos
+            // 
+            this.grbIngresoDeDatos.Controls.Add(this.textNombre);
+            this.grbIngresoDeDatos.Controls.Add(this.textApellido);
+            this.grbIngresoDeDatos.Controls.Add(this.buttonAgregar);
+            this.grbIngresoDeDatos.Controls.Add(this.lblNombre);
+            this.grbIngresoDeDatos.Controls.Add(this.lblApellido);
+            this.grbIngresoDeDatos.Location = new System.Drawing.Point(12, 12);
+            this.grbIngresoDeDatos.Name = "grbIngresoDeDatos";
+            this.grbIngresoDeDatos.Size = new System.Drawing.Size(352, 429);
+            this.grbIngresoDeDatos.TabIndex = 7;
+            this.grbIngresoDeDatos.TabStop = false;
+            this.grbIngresoDeDatos.Text = "Ingreso de Datos";
+            // 
+            // grbElementos
+            // 
+            this.grbElementos.Controls.Add(this.lbIngresoNombres);
+            this.grbElementos.Location = new System.Drawing.Point(406, 12);
+            this.grbElementos.Name = "grbElementos";
+            this.grbElementos.Size = new System.Drawing.Size(323, 429);
+            this.grbElementos.TabIndex = 8;
+            this.grbElementos.TabStop = false;
+            this.grbElementos.Text = "Elementos";
             // 
             // FormEjercicio2
             // 
@@ -99,19 +125,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(769, 487);
-            this.Controls.Add(this.lblIngresoDeDatos);
-            this.Controls.Add(this.buttonAgregar);
-            this.Controls.Add(this.textApellido);
-            this.Controls.Add(this.textNombre);
-            this.Controls.Add(this.lblApellido);
-            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.grbElementos);
+            this.Controls.Add(this.grbIngresoDeDatos);
             this.Name = "FormEjercicio2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Nombre y apellido";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormEjercicio2_FormClosed);
             this.Load += new System.EventHandler(this.FormEjercicio2_Load);
+            this.grbIngresoDeDatos.ResumeLayout(false);
+            this.grbIngresoDeDatos.PerformLayout();
+            this.grbElementos.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -122,6 +146,8 @@
         private System.Windows.Forms.TextBox textNombre;
         private System.Windows.Forms.TextBox textApellido;
         private System.Windows.Forms.Button buttonAgregar;
-        private System.Windows.Forms.Label lblIngresoDeDatos;
+        private System.Windows.Forms.ListBox lbIngresoNombres;
+        private System.Windows.Forms.GroupBox grbIngresoDeDatos;
+        private System.Windows.Forms.GroupBox grbElementos;
     }
 }
--- TP1-Grupo18/FormEjercicio2.cs
+++ TP1-Grupo18/FormEjercicio2.cs
@@ -36,7 +36,47 @@ namespace TP1_Grupo18
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //Valida que no esté en blanco
+            if (textNombre.Text.Trim() == "" && textApellido.Text.Trim() != "")
+            {
+                MessageBox.Show("Por favor ingrese su nombre", "Ventana emergente");
+            }
+            else if (textNombre.Text.Trim() != "" && textApellido.Text.Trim() == "")
+            {
+                MessageBox.Show("Por favor ingrese su apellido", "Ventana emergente");
+            }
+            else if (textNombre.Text.Trim() == "" && textApellido.Text.Trim() == "")
+            {
+                MessageBox.Show("Por favor ingrese su nombre y apellido", "Ventana emergente");
+            }
+            else
+            {
+                string nuevoItem = textNombre.Text.Trim().ToLower() + " " + textApellido.Text.Trim().ToLower();
+                bool existe = false;
 
+                //se compara cada item de la lista con la copia
+                foreach (string item in lbIngresoNombres.Items)
+                {
+                    //si encuentra coincidencia cambia de estado la bandera y detiene el bucle
+                    if (item.ToLower() == nuevoItem)
+                    {
+                        existe = true;
+
+                        break;
+                    }
+                }
+
+                //se verifica que el nombre ya fue ingresado
+                if (existe==true)
+                {
+                    MessageBox.Show("El nombre ya fue ingresado", "Atencion");
+                }
+                //se agrega el contenido del texto original a la lista de items
+                else
+                {
+                    lbIngresoNombres.Items.Add(nuevoItem);
+                }
+            }
         }
 
         private void label1_Click_1(object sender, EventArgs e)
